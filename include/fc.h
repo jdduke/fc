38426dcd6b55b64edf8e2d1b5df6840456056621
Jared D <glorindel@gmail.com>
2012-01-23 18:58:00 -0800

    Code cleanup and some simplification.

--- include/fc.h
+++ include/fc.h
@@ -57,9 +57,34 @@ inline auto operator+(F f, G* g) -> typename composed_traits<F, typename make_fu
 template<typename F, typename G>
 class composed_base {
 public:
+  enum {
+    arity = function_traits<G>::arity
+  };
+
   template< typename G2 >
   auto with(G2 g2) const -> typename composed_traits< typename composed_traits<F,G>::type, G2>::type {
     return compose(compose(f,g), g2);
+  }
+
+  /*
+  auto operator()() -> typename std::enable_if<arity==0, typename compound_result0<F,G>::type>::type {
+    return f(g()); 
+  }
+  */
+
+  template<typename T1>
+  auto operator()(const T1& t) -> typename compound_result1<F,G,T1>::type {
+    return f(g(t));
+  }
+
+  template<typename T1, typename T2>
+  auto operator()(const T1& t1, const T2& t2) -> typename compound_result2<F,G,T1,T2>::type {
+    return f(g(t1,t2));
+  }
+
+  template<typename T1, typename T2, typename T3>
+  auto operator()(const T1& t1, const T2& t2, const T3& t3) -> typename compound_result3<F,G,T1,T2,T3>::type {
+    return f(g(t1,t2,t3));
   }
 
 protected:
@@ -72,11 +97,15 @@ protected:
   G g;
 };
 
+///////////////////////////////////////////////////////////////////////////
+
 template<typename F, typename G0, typename G1>
 class composed_base2 : public composed_base<F,G0> {
 public:
   enum {
-    arity_0 = function_traits<G0>::arity;
+    arity_0 = function_traits<G0>::arity,
+    arity_1 = function_traits<G1>::arity,
+    arity_c = arity_0 + arity_1
   };
 
 protected:
@@ -90,58 +119,21 @@ protected:
 
 ///////////////////////////////////////////////////////////////////////////
 
-template<typename F, typename G, size_t argFC, size_t ArgGC>
+template<typename F, typename G, size_t ArgFC, size_t ArgGC>
 class composed : public composed_base<F,G> {
+public:
+  composed(F f_,   G g_)   : composed_base<F,G>(f_,g_) { }
+  composed(F&& f_, G&& g_) : composed_base<F,G>(std::forward(f_),std::forward(g_)) { }
+};
+
+template<typename F, typename G, size_t ArgFC>
+class composed<F,G,ArgFC,0> : public composed_base<F,G> {
 public:
   composed(F f_,   G g_)   : composed_base<F,G>(f_,g_) { }
   composed(F&& f_, G&& g_) : composed_base<F,G>(std::forward(f_),std::forward(g_)) { }
 
   auto operator()() -> typename compound_result0<F,G>::type { return f(g()); }
 };
-
-///////////////////////////////////////////////////////////////////////////
-
-template<typename F, typename G, size_t ArgFC>
-class composed<F,G,ArgFC,1> : public composed_base<F,G> {
-public:
-  explicit composed(F f_,   G g_)   : composed_base<F,G>(f_,g_) { }
-  explicit composed(F&& f_, G&& g_) : composed_base<F,G>(std::forward(f_),std::forward(g_)) { }
-
-  template<typename T1>
-  auto operator()(const T1& t) -> typename compound_result1<F,G,T1>::type {
-    return f(g(t));
-  }
-};
-
-///////////////////////////////////////////////////////////////////////////
-
-template<typename F, typename G, size_t ArgFC>
-class composed<F,G,ArgFC, 2> : public composed_base<F,G> {
-public:
-  explicit composed(F f_,   G g_)   : composed_base<F,G>(f_,g_) { }
-  explicit composed(F&& f_, G&& g_) : composed_base<F,G>(std::forward(f_),std::forward(g_)) { }
-
-  template<typename T1, typename T2>
-  auto operator()(const T1& t1, const T2& t2) -> typename compound_result2<F,G,T1,T2>::type {
-    return f(g(t1,t2));
-  }
-};
-
-///////////////////////////////////////////////////////////////////////////
-
-template<typename F, typename G, size_t ArgFC>
-class composed<F,G,ArgFC,3> : public composed_base<F,G> {
-public:
-  explicit composed(F f_,   G g_)   : composed_base<F,G>(f_,g_) { }
-  explicit composed(F&& f_, G&& g_) : composed_base<F,G>(std::forward(f_),std::forward(g_)) { }
-
-  template<typename T1, typename T2, typename T3>
-  auto operator()(const T1& t1, const T2& t2, const T3& t3) -> typename compound_result3<F,G,T1,T2,T3>::type {
-    return f(g(t1,t2,t3));
-  }
-};
-
-///////////////////////////////////////////////////////////////////////////
 
 }
 
