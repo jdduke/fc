199386c59729949c59fc6bc777286f81df3c607f
Jared D <glorindel@gmail.com>
2012-02-02 19:38:50 -0800

    Updates... no more use for make_function

--- include/fc_utils.h
+++ include/fc_utils.h
@@ -31,12 +31,27 @@ template<typename F, typename G0, typename G1, typename G2, size_t ArgG> class c
 
 ///////////////////////////////////////////////////////////////////////////
 
+template <typename T>
+struct make_function_traits {
+  typedef std::function<typename std::enable_if<std::is_function<T>::value, T>::type> type;
+};
+
+template<typename T>
+auto make_function(T *t) -> typename make_function_traits<T>::type {
+  return t;
+}
+
+///////////////////////////////////////////////////////////////////////////
+
 template <typename T> struct function_traits;
 template <typename T0, typename T1, typename T2> struct function_traits2;
 template <typename T0, typename T1, typename T2, typename T3> struct function_traits3;
 
 template <typename T>
 struct function_traits : public function_traits< decltype( &T::operator() ) > {};
+
+template <typename T>
+struct function_traits<T*> : public function_traits< typename make_function_traits<T>::type > { };
 
 template <typename T0, typename T1, typename T2>
 struct function_traits2 {
@@ -71,30 +86,45 @@ template <typename C, typename R, typename T0>
 struct function_traits<R(C::*)(T0) const> {
 	static const size_t arity = 1;
 	typedef R result_type;
+  typedef T0 t0_type;
 };
 
 template <typename C, typename R, typename T0, typename T1>
 struct function_traits<R(C::*)(T0,T1) const> {
 	static const size_t arity = 2;
 	typedef R result_type;
+  typedef T0 t0_type;
+  typedef T1 t1_type;
 };
 
 template <typename C, typename R, typename T0, typename T1, typename T2>
 struct function_traits<R(C::*)(T0,T1,T2) const> {
 	static const size_t arity = 3;
 	typedef R result_type;
+  typedef T0 t0_type;
+  typedef T1 t1_type;
+  typedef T2 t2_type;
 };
 
 template <typename C, typename R, typename T0, typename T1, typename T2, typename T3>
 struct function_traits<R(C::*)(T0,T1,T2,T3) const> {
 	static const size_t arity = 4;
 	typedef R result_type;
+  typedef T0 t0_type;
+  typedef T1 t1_type;
+  typedef T2 t2_type;
+  typedef T3 t3_type;
 };
 
 template <typename C, typename R, typename T0, typename T1, typename T2, typename T3, typename T4>
 struct function_traits<R(C::*)(T0,T1,T2,T3,T4) const> {
   static const size_t arity = 5;
   typedef R result_type;
+  typedef T0 t0_type;
+  typedef T1 t1_type;
+  typedef T2 t2_type;
+  typedef T3 t3_type;
+  typedef T4 t4_type;
 };
 
 template <typename C, typename R>
@@ -107,30 +137,45 @@ template <typename C, typename R, typename T0>
 struct function_traits<R(C::*)(T0)> {
   static const size_t arity = 1;
   typedef R result_type;
+  typedef T0 t0_type;
 };
 
 template <typename C, typename R, typename T0, typename T1>
 struct function_traits<R(C::*)(T0,T1)> {
   static const size_t arity = 2;
   typedef R result_type;
+  typedef T0 t0_type;
+  typedef T1 t1_type;
 };
 
 template <typename C, typename R, typename T0, typename T1, typename T2>
 struct function_traits<R(C::*)(T0,T1,T2)> {
   static const size_t arity = 3;
   typedef R result_type;
+  typedef T0 t0_type;
+  typedef T1 t1_type;
+  typedef T2 t2_type;
 };
 
 template <typename C, typename R, typename T0, typename T1, typename T2, typename T3>
 struct function_traits<R(C::*)(T0,T1,T2,T3)> {
   static const size_t arity = 4;
   typedef R result_type;
+  typedef T0 t0_type;
+  typedef T1 t1_type;
+  typedef T2 t2_type;
+  typedef T3 t3_type;
 };
 
 template <typename C, typename R, typename T0, typename T1, typename T2, typename T3, typename T4>
 struct function_traits<R(C::*)(T0,T1,T2,T3,T4)> {
   static const size_t arity = 5;
   typedef R result_type;
+  typedef T0 t0_type;
+  typedef T1 t1_type;
+  typedef T2 t2_type;
+  typedef T3 t3_type;
+  typedef T4 t4_type;
 };
 
 
@@ -253,18 +298,6 @@ struct composed_traits {
 };
 
 #endif /* FC_VARIADIC */
-
-///////////////////////////////////////////////////////////////////////////
-
-template <typename T>
-struct make_function_traits {
-	typedef std::function<typename std::enable_if<std::is_function<T>::value, T>::type> type;
-};
-
-template<typename T>
-auto make_function(T *t) -> typename make_function_traits<T>::type {
-	return t;
-}
 
 ///////////////////////////////////////////////////////////////////////////
 
